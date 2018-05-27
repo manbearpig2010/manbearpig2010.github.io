5e23fa0b353efcde1c6cfcf0e29361c1067a779d
manbearpig2010 <krauseconor@gmail.com>
2018-05-27 10:11:44 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ top_pic.setAttribute("src","https://i.imgur.com/hnkOIpI.jpg");
 }
 });
 
-  $(".hotel_panel").on('touchend', function(){
+  $(".hotel_panel").on('click', function(){
    if($(this).find("h3").text() == "View Offers"){
 		$(this).find("h3").text("Hide Offers");
 		}
